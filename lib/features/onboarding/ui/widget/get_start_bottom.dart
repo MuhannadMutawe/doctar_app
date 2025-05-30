83e679c09450b575ddc616b75131e7818c81f6de
muhannad mutawe <muhannadrm208@gamil.com>
2025-05-30 16:41:55 +0300

    _set up flavors for android . _added main staging file. _added main production file. _edit in some navigator methods

--- lib/features/onboarding/ui/widget/get_start_bottom.dart
+++ lib/features/onboarding/ui/widget/get_start_bottom.dart
@@ -10,7 +10,7 @@ class GetStartBottom extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return TextButton(
-      onPressed: () => context.pushReplacement(Routes.loginScreen),
+      onPressed: () => context.pushNamedAndRemoveUntil(Routes.loginScreen,predicate: (route) => route.isCurrent,),
       style: ButtonStyle(
         backgroundColor: WidgetStateProperty.all(AppColors.mainBlue),
         tapTargetSize: MaterialTapTargetSize.shrinkWrap,
